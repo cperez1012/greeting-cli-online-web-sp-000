2c39660f328a0b314dbf256adcc2cb3579c5effb
Christian Perez <perezchristian1012@gmail.com>
2019-07-10 16:02:54 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!
 def greeting(name)
-puts "Hello #{name}. It's nice to meet you."
-print (Name.strip('/t   ')
+  puts "Hello #{name}. It's nice to meet you."
 end
+
